4e246ced43ba21a4e34247b4b52fafe9b3e87bc9
geniusthomas3 <52883542+geniusthomas3@users.noreply.github.com>
2020-06-28 01:29:59 +0900

    BACKUP.2020-06-28_ 1:29:59.26

--- unitsoft/virus/main.cpp
+++ unitsoft/virus/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+/*#include <stdio.h>
+#include <queue>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(){
+    int n,k;
+    scanf("%d %d",&n,&k);
+    for(int i=0;i<k;i++){
+        scanf("%d %d",&,&);
+    }
+    queue <int> que;
+
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+    char data[51]="";
+scanf("%s", data);
+printf("%s",data);    return 0;
 }
