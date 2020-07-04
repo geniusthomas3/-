d6a38456b2ac5894cb6b591c4e1a2c328e0e8c29
geniusthomas3 <52883542+geniusthomas3@users.noreply.github.com>
2020-07-04 10:25:13 +0900

    BACKUP.2020-07-04_10:25:13.42

--- unitsoft/virus/main.cpp
+++ unitsoft/virus/main.cpp
@@ -1,23 +1,34 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <queue>
+#include <vector>
 
 using namespace std;
+int check[101];
 
 int main(){
-    int n,k;
+
+    int n,k,a,b;
     scanf("%d %d",&n,&k);
+    vector<vector<int>>g(n+1);
     for(int i=0;i<k;i++){
-        scanf("%d %d",&,&);
+        scanf("%d %d",&a,&b);
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-    queue <int> que;
-
-}
-*/
-
-#include <stdio.h>
-
-int main(){
-    char data[51]="";
-scanf("%s", data);
-printf("%s",data);    return 0;
+    queue <int> q;
+    q.push(1);
+    check[1]=1;
+    int cnt=0;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        for(auto x : g[node]){
+            if(check[x]==0){
+                check[x]=1;
+                cnt++;
+                q.push(x);
+            }
+        }
+    }
+    printf("%d",cnt);
 }
